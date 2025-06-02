c3f95171d5800ba188dac3ef272da07b64e4ec73
jimmypuli <armg296@gmail.com>
2025-06-01 20:12:53 -0400

    Working version
    Gets description and title. Company done manually.

--- sb.py
+++ sb.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 from seleniumbase import SB
-# import json
+import json
 
-# reviews_data = []
+reviews_data = []
 
 with SB(uc=True, test=True, locale="en") as sb:
     url = "https://www.indeed.com/cmp/Walmart/reviews"
@@ -15,20 +15,19 @@ with SB(uc=True, test=True, locale="en") as sb:
     reviewList = soup.find('ul', class_='css-u74ql7 eu4oa1w0')
     
     for child in reviewList: 
-        title = child.find('span', class_='css-15r9gu1 eu4oa1w0')
-        description = child.find_next('span', class_='css-15r9gu1 eu4oa1w0')
-        if title and description: 
-            print(title)
-            print(description)
-            # review = {
-            #     'title' : title.text.strip(),
-            #     'description': description.text.strip(),
-            #     'company' : 'Walmart', 
-            # }
-            # reviews_data.append(review)
+        spans = child.find_all('span', class_='css-15r9gu1 eu4oa1w0')
+        if len(spans) >= 2: 
+            title = spans[0]
+            description = spans[1]
+            review = {
+                'title' : title.text.strip(),
+                'description': description.text.strip(),
+                'company' : 'Walmart', 
+            }
+            reviews_data.append(review)
         
-# with open('reviews.json', 'w', encoding='utf-8') as f: 
-#     json.dump(reviews_data, f, indent=2, ensure_ascii=False)
+with open('reviews.json', 'w', encoding='utf-8') as f: 
+    json.dump(reviews_data, f, indent=2, ensure_ascii=False)
 
 
 
